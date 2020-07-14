b881c2b35afb6c3422c9c977af6af2bb6d2da3fe
Shihab <67054762+internet-help-zone-shihab@users.noreply.github.com>
2020-07-14 14:45:15 +0600

    Update instack.py

--- instack.py
+++ instack.py
@@ -1,5 +1,5 @@
-# Created by Mr Hacker xD (https://github.com/faizanwahla)
-# Instagram : @mr._.faizu01
+# Created by Mohammad shihab Uddin (https://github.com/internet-help-zone-shihab)
+# Facebook:- https://www.facebook.com/shihabuddin.shihab.1865
 
 import marshal,zlib,base64
 exec(marshal.loads(zlib.decompress(base64.b32decode("PCOELDSLBIBDCDEGSN4YBCQH5FFC6IBCZCQIFTDGLS4RXW7CQPEERE46YCBWXIUCEGHS7YJHRGDZ6VI2FMGXNGUCHIBACHAVBLMGYI6ABUQZIEBKRACTYEKRAWO2WVL4PWU3KYWI2IT6DMOSNS7N7LM3WZV3542LERZKLESMVTG2ORY2PRSBMO7LPORA6HCSRZ3B57UJBN56OLBDFWF7WEBSYWS62YOTJ6Y4MN552ERN4==="))))
